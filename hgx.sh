fe262ccd1689602d28dc6972b62416820efead58
Matoro17 <gabrielsilvadeazevedo@gmail.com>
2025-04-02 17:10:59 -0300

    Fix hgx slurm script

--- hgx.sh
+++ hgx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -p batch # partition (queue)
-#SBATCH --gpus G # change G by the number of GPU to use
+#SBATCH --gpus 1 # change G by the number of GPU to use
 #SBATCH -o slurm.%N.%j.out # STDOUT
 #SBATCH -e slurm.%N.%j.err # STDERR
 
