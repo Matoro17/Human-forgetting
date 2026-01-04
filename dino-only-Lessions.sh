2e684bfc9dd9a547278ca9daaae85c3345cb4670
Matoro17 <gabrielsilvadeazevedo@gmail.com>
2026-01-04 17:43:31 -0300

    Fix slurm script nametag

--- dino-only-Lessions.sh
+++ dino-only-Lessions.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH -p batch # partition (queue)
 #SBATCH --gpus 2 # change G by the number of GPU to use
-#SBATCH -o 2026-slurm.%N.%j.out # STDOUT
-#SBATCH -e 2026-slurm.%N.%j.err # STDERR
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
 
 python3 -u /home/gabrielazevedo/Human-forgetting/models/experiment_multiclass_onlylessions.py
